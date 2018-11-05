5c4cfe8e772abf41f0435262a43f2d382dda5cf6
Erlend Røsjø <erlend@erlend.net>
2018-11-05 20:37:46 +0100

    Ex 4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './reduxStore';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/todosReducer.js
@@ -0,0 +1,8 @@
+const todosReducer = (todos = [], action) => {
+    switch (action.type) {
+      default:
+        return todos;
+    }
+  };
+  
+  export default todosReducer;
